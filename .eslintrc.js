5897923ba5ade3da52e4883aa87aa1e94613e8d2
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-03-24 12:45:48 +0900

    🔧 🔥 絶対パスに変更し、不要な記述を削除 (#1)

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:react/jsx-runtime",
     "prettier",
   ],
   parser: "@typescript-eslint/parser",
